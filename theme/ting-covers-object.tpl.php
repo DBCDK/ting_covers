76d4ebd346c0133e03de3c705e1bb1abd209a65f
Anders Tolborg <anders@tolborg.dk>
2014-06-28 17:08:14 +0200

    Retheme work cover. This module seems to contain A LOT of legacy code!

--- theme/ting-covers-object.tpl.php
+++ theme/ting-covers-object.tpl.php
@@ -1,12 +1,19 @@
+<?php
+/**
+ * @file
+ * Renders the HTML for a work cover
+ */
+?>
 
-  <div class="work-cover">
-    <div class="work-cover-image<?php print $elements['#no_image_class'] ?>">
-      <div class="<?php print( implode(' ', $elements['#classes'])); ?>">
-        <?php print $elements['#image'] ?>
-      </div>
-    </div>
-    <div class="work-cover-selector clearfix">
-      <?php print $elements['#front_cover_large_link'] ?>
-      <?php print $elements['#back_cover_large_link'] ?>
-    </div>
+<div class="work-cover">
+
+  <div class="work-cover__image">
+    <?php print $elements['#image'] ?>
   </div>
+
+  <div class="work-cover__selectors">
+    <?php print $elements['#front_cover_large_link'] ?>
+    <?php print $elements['#back_cover_large_link'] ?>
+  </div>
+
+</div>
